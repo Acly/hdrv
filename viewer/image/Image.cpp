8ade2b414a572abee8d64baa8fd3b4b4c177aa81
Nikolas Schmitt <nikolas.schmitt@3ds.com>
2021-01-08 18:05:20 +0100

    Fixed tiled EXR with odd resolutions (TinyEXR's tile pixel  buffers are apparently always as large as the global tile resolution, not the actual resolution of the current tile)

--- viewer/image/Image.cpp
+++ viewer/image/Image.cpp
@@ -424,10 +424,11 @@ Result<Image> Image::loadEXR(std::byte const* inputMemory, size_t size)
       resultLayers[l].channels += 1;
       resultLayers[l].display = guessDisplay(layer, exrHeaders[layer.part]->channels[channel.index].pixel_type);
 
-      auto copyTile = [&](int offsetX, int offsetY, int tileWidth, int tileHeight, unsigned char const* pixels) {
+      auto copyTile = [&](int offsetX, int offsetY, int tileWidth, int tileHeight, int tileStride,
+                          unsigned char const* pixels) {
         for (int y = 0; y < tileHeight; ++y) {
           for (int x = 0; x < tileWidth; ++x) {
-            int srcCoord = y * tileWidth + x;
+            int srcCoord = y * tileStride + x;
             int dstCoord = (height - offsetY - y - 1) * width + (offsetX + x);
             auto src = pixels + srcCoord * sizeof(float);
             auto dst = resultData.data() + dataOffset
@@ -438,13 +439,16 @@ Result<Image> Image::loadEXR(std::byte const* inputMemory, size_t size)
       };
 
       if (img.images) {
-        copyTile(0, 0, width, height, img.images[channel.index]);
+        copyTile(0, 0, width, height, width, img.images[channel.index]);
       } else {
         for (int t = 0; t < img.num_tiles; ++t) {
           auto& tile = img.tiles[t];
+          if (tile.level_x != 0 || tile.level_y != 0) {
+            continue; // ignore mipmap levels
+          }
           int x = tile.offset_x * img.tiles[0].width;
           int y = tile.offset_y * img.tiles[0].height;
-          copyTile(x, y, tile.width, tile.height, tile.images[channel.index]);
+          copyTile(x, y, tile.width, tile.height, img.tiles[0].width, tile.images[channel.index]);
         }      
       }
     }
